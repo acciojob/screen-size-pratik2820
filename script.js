6e33cdea1f33e47dfba198092834b164ecf693e3
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-05-25 10:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ var w = document.documentElement.clientWidth;
 var h = document.documentElement.clientHeight;
         
 // put the result into a h1 tag
- document.getElementById('wh').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
+ document.getElementById("sizeInfo").innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
 }
