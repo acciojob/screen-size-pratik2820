90dede646d22b2260ca250e80ea508e4069e88c0
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-05-25 10:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const sizeElement = document.createElement("h1");
+sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+document.body.append(sizeElement);
+
+window.onresize = function(){
+	// console.log(window.innerHeight,window.innerHeight)
+	sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+	document.body.append(sizeElement);
+}
