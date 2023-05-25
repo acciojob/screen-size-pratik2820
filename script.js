bcd9e9ce50c8337745ffe980b44a6f2015ea5bc7
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-05-25 10:18:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 //your JS code here. If required.
+// we can also write in this way but code was not getting submitted so treied the other way
+// const sizeElement = document.createElement("h1");
+// sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+// document.body.append(sizeElement);
 
-const sizeElement = document.createElement("h1");
-sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
-document.body.append(sizeElement);
+// window.onresize = function(){
+// 	// console.log(window.innerHeight,window.innerHeight)
+// 	sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+// 	document.body.append(sizeElement);
+// }
 
-window.onresize = function(){
-	// console.log(window.innerHeight,window.innerHeight)
-	sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
-	document.body.append(sizeElement);
+
+// other way to write the same code
+
+function getSize()
+{
+var w = document.documentElement.clientWidth;
+var h = document.documentElement.clientHeight;
+        
+// put the result into a h1 tag
+ document.getElementById('wh').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
 }
